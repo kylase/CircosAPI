2384fecbc07ec190b8bb9678b00ab18d97f02aab
Kyle <kylase@outlook.com>
2013-03-14 16:17:16 +0800

    Descriptive variable names till Example 5

--- test.pl
+++ test.pl
@@ -26,38 +26,45 @@ $c->ticks($ticks);
 
 # 3. Selection and scale
 
-$b->chromosomes_display_default('no');
-$b->chromosomes('/hs[1-4]$/');
-$b->chromosomes_scale('hs1=0.5r,/hs[234]/=0.5rn');
-$b->chromosomes_reverse('/hs[234]/');
-$b->chromosomes_color('hs1=red,hs2=orange,hs3=green,hs4=blue');
-$b->chromosomes_radius('hs4:0.9r');
+$base_hg19->chromosomes_display_default('no');
+$base_hg19->chromosomes('/hs[1-4]$/');
+$base_hg19->chromosomes_scale('hs1=0.5r,/hs[234]/=0.5rn');
+$base_hg19->chromosomes_reverse('/hs[234]/');
+$base_hg19->chromosomes_color('hs1=red,hs2=orange,hs3=green,hs4=blue');
+$base_hg19->chromosomes_radius('hs4:0.9r');
 
 #print $c->compile;
 
 # 4. Links and rules
 
-my $l = Link->new(file => 'data/5/segdup.txt', radius => '0.8r', bezier_radius => '0r', color => 'black_a4', thickness => 2);
-$l->addRule(Rule->new(condition => 'var(intrachr)', params => { show => 'no' } ));
-$l->addRule(Rule->new(condition => '1', params => { color => 'eval(var(chr2))', flow => 'continue' }));
-$l->addRule(Rule->new(condition => 'from(hs1)', params => { radius1 => '0.99r' }));
-$l->addRule(Rule->new(condition => 'to(hs1)', params => { radius2 => '0.99r' }));
-$c->addLink($l);
+my $segdup_link_black = Link->new(file => 'data/5/segdup.txt', radius => '0.8r', bezier_radius => '0r', color => 'black_a4', thickness => 2);
+
+$segdup_link_black->addRule(Rule->new(condition => 'var(intrachr)', params => { show => 'no' } ));
+$segdup_link_black->addRule(Rule->new(condition => '1', params => { color => 'eval(var(chr2))', flow => 'continue' }));
+
+my $rule_fromhs1 = Rule->new(condition => 'from(hs1)', params => { radius1 => '0.99r' });
+my $rule_tohs1 = Rule->new(condition => 'to(hs1)', params => { radius2 => '0.99r' });
+$segdup_link_black->addRule($rule_fromhs1, $rule_tohs1);
+
+$c->addLink($segdup_link_black);
+
+# print $c->compile;
 
 # 5. Histogram
 
-my $hist_data = { t => 'histogram', 
+my $segdup_hist_params = { t => 'histogram', 
                 file => 'data/5/segdup.hs1234.hist.txt', 
                 r1 => '0.88r', r0 => '0.81r', 
                 fill_color => 'vdgrey', 
-                extend_bin => 'no'};
+                extend_bin => 'no' };
 
-my $p = Plot->new($hist_data);
-my $r = Rule->new(condition => 'on(hs1)', params => { show => 'no' } );
-my $p2 = Plot->new(t => 'histogram', file => 'data/5/segdup.hs1234.stacked.txt', r1 => '0.99r', r0 => '0.92r', fill_color => 'hs1,hs2,hs3,hs4', orientation => 'in');
-$p->addRule($r);
-$p2->addRule($r);
-$c->addPlot($p, $p2);
+my $inner_plot_segdup_hist = Plot->new($segdup_hist_params);
+my $outer_plot_segdup_stacked = Plot->new(t => 'histogram', file => 'data/5/segdup.hs1234.stacked.txt', r1 => '0.99r', r0 => '0.92r', fill_color => 'hs1,hs2,hs3,hs4', orientation => 'in');
+
+my $rule_onhs1_noshow = Rule->new(condition => 'on(hs1)', params => { show => 'no' } );
+$inner_plot_segdup_hist->addRule($rule_onhs1_noshow);
+$outer_plot_segdup_stacked->addRule($rule_onhs1_noshow);
+$c->addPlot($inner_plot_segdup_hist, $outer_plot_segdup_stacked);
 
 # 6. Axes and Background
 
